c2feeb15af630d526e7c366f699ae8c74a391b61
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-07-01 16:13:39 -0700

    Print Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints every decimal digit
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
