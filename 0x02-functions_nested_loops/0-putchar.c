01f869d1cced0c262afead25ca9701a327360d7d
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-07-05 08:52:47 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,14 @@
 
 int main(void)
 {
-	write(2, "_putchar\n", 59);
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 	return (0);
 }
