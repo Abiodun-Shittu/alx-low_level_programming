15590b5f321b842f58f5db01467a4bbc0de4b979
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-07-06 00:17:33 +0100

    10 X alphabet && git push

---
+++ 2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
