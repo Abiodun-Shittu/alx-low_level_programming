f8f54fdd17b3ce0c4ad71d6e0c589e3fa4b1905d
Abiodun Gafar Shittu <69143577+Abiodun-Shittu@users.noreply.github.com>
2022-07-06 00:35:02 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,19 @@
 #include "main.h"
-
 /**
- * print_alphabet - print the alphabet in lowercase
- *
- * Return: Always 0.
- */
-void print_alphabet(void)
+  * print_alphabet_x10 - repeats the print_alphabet 10 times.
+  * Return: Nothing.
+  */
+void print_alphabet_x10(void)
 {
-char c;
+	int i;
+	char j;
 
-for (c = 'a'; c <= 'z'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
+	for (i = 0; i < 10; ++i)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
 }
