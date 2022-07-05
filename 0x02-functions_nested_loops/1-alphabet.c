0ebf98af8d9767c0f75ae871aac32389b6fac3de
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-07-05 11:30:09 -0700

    Print Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - print the alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
