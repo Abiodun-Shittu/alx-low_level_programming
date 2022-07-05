9e555a13e5521ed0dfdd1899d7b529f6ad55f601
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-07-05 08:22:49 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *main - Program that prints _putchar
+ *followed by a new line
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(2, "_putchar\n", 59);
+	return (0);
+}
