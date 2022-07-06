d52d09457fa1f53f52e2ccb07bbca55fdd1849a4
Abiodun Gafar Shittu <69143577+Abiodun-Shittu@users.noreply.github.com>
2022-07-06 04:34:32 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* jack_bauer - Prints every minute of the day of Jack Bauer, 
+* jack_bauer - Prints every minute of the day of Jack Bauer,
 * starting from 00:00 to 23:59.
 */
 void jack_bauer(void)
