ef805a62efb9c101d79be71ff58f73c4397bf78a
Abiodun Gafar Shittu <69143577+Abiodun-Shittu@users.noreply.github.com>
2022-07-06 04:45:12 +0100

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two numbers together.
+ * @i: The first number
+ * @j: The second number
+ * Return: the sum of two numbers.
+ */
+ 
+int add(int i, int j)
+{
+	return (i + j);
+}
