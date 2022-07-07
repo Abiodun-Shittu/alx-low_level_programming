d417c8b9e04e288e568e4878f93ccee029ac5fb1
Abiodun Gafar Shittu <69143577+Abiodun-Shittu@users.noreply.github.com>
2022-07-07 12:24:12 +0100

    print remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+
 #include "main.h"
+
 /**
 * print_remaining_days - takes a date and prints how many days are
 * left in the year, taking leap years into account
@@ -18,14 +20,13 @@ if (month > 2 && day >= 60)
 day++;
 }
 
-
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
 if (month == 2 && day == 60)
-}
+{
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
