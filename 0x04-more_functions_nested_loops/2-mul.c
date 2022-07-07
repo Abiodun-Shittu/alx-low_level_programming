a3147bd81050a8546a178134f855728b1ca2c647
Abiodun Gafar Shittu <69143577+Abiodun-Shittu@users.noreply.github.com>
2022-07-08 00:16:15 +0100

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integer
+ * @a: integer type number
+ * @b: integer type number
+ * Return: multiply
+ */
+
+int mul(int a, int b)
+{
+int multiply = a * b;
+return (multiply);
+}
