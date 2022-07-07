20934b77b219fe242dbee5d15cc5fa172de8fd15
Abiodun Gafar Shittu <69143577+Abiodun-Shittu@users.noreply.github.com>
2022-07-08 00:00:08 +0100

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,10 @@
 
 /**
  * _isupper - check for upper case letter
- * @c : character to check the case
+ * @c: character to check the case
  * Return: 1 if c is upper case and 0 otherwise
  */
- 
+
 int _isupper(int c)
 {
 if ((c >= 65) && (c <= 90))
