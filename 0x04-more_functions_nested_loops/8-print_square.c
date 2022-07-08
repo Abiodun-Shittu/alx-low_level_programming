4ba69d1d9c2c569288b966ca496de71ba1c43201
Abiodun Gafar Shittu <69143577+Abiodun-Shittu@users.noreply.github.com>
2022-07-08 04:56:18 +0100

    Print Square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,19 +9,19 @@
 void print_square(int size)
 {
 int i, j;
+
 if (size <= 0)
 {
 _putchar('\n');
 }
 else
 {
-for ( i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
-for ( j = 0; j < size; j++)
+for (j = 0; j < size; j++)
 {
 _putchar('#');
 }
-_putchar('#');
 _putchar('\n');
 }
 }
