8840973a03feba46d737c94824eaa3a593970b5c
Abiodun Gafar Shittu <69143577+Abiodun-Shittu@users.noreply.github.com>
2022-07-08 05:03:26 +0100

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,19 +17,23 @@ for ( i = 1; i <= 100; i++)
 {
 if ((i % 3 == 0) && (i % 5 == 0))
 {
-printf("%s", "FizzBuzz ");
+printf("%s", "FizzBuzz");
 }
 else if (i % 3 == 0)
 {
-printf("%s ", "Fizz");
+printf("%s", "Fizz");
 }
 else if (i % 5 == 0)
 {
-printf("%s ", "Buzz");
+printf("%s", "Buzz");
 }
 else
 {
-printf("%d ", i);
+printf("%d", i);
+}
+if (i != 100)
+{
+printf(" ");
 }
 }
 printf("\n");
