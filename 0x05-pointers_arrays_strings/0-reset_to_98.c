e6bfe484acacbc48fe9cb7719e5c3157ce752935
Abiodun Gafar Shittu <69143577+Abiodun-Shittu@users.noreply.github.com>
2022-07-13 00:23:54 +0100

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,7 @@
 /**
  * reset_to_98 - function that takes a pointer to an int as parameter
  * and updates the value it points to to 98.
+ * @n: the variable pointer at n
  */
 
 void reset_to_98(int *n)
