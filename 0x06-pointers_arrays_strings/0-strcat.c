235f0dff5d5bbb640af9abdb1d714f541c87e7c5
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-07-15 00:36:47 +0100

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,10 +17,10 @@ for ( i = 0; dest[i] != '\0'; i++)
 {
 
 }
-for ( j = 0; src[j] != '\0'; j++)
+for ( j = 0; src[j] != '\0'; j++, i++)
 {
 dest[i] = src[j];
-i++;
 }
+dest[i] = '\0';
 return (dest);
 }
