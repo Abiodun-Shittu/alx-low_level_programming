2b6466106815ff41891b6bcd4293faa46b838032
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-07-15 00:38:19 +0100

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,14 +13,14 @@ char *_strcat(char *dest, char *src)
 {
 int i, j;
 
-for ( i = 0; dest[i] != '\0'; i++)
+for (i = 0; dest[i] != '\0'; i++)
 {
 
 }
-for ( j = 0; src[j] != '\0'; j++, i++)
+for (j = 0; src[j] != '\0'; j++)
 {
 dest[i] = src[j];
+i++;
 }
-dest[i] = '\0';
 return (dest);
 }
