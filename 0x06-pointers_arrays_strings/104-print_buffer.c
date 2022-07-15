83256e05ff491ae18ef9405e19f50e616ef7e352
Abiodun Gafar Shittu <69143577+Abiodun-Shittu@users.noreply.github.com>
2022-07-15 08:48:48 +0100

    Noise is a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,33 +8,33 @@
  */
 void print_buffer(char *b, int size)
 {
-int byte, i;
+	int byte, i;
 
-for (byte = 0; byte < size; byte += 10)
-{
-printf("%08x: ", byte);
-for (i = 0; i < 10; i++)
-{
-if ((i + byte) >= size)
-printf("  ");
-else
-printf("%02x", *(b + i + byte));
-if ((i % 2) != 0 && i != 0)
-printf(" ");
-}
-for (i = 0; i < 10; i++)
-{
-if ((i + byte) >= size)
-break;
-else if (*(b + i + byte) >= 31 && *(b + i + byte) <= 126)
-printf("%c", *(b + i + byte));
-else
-printf(".");
-}
-if (byte >= size)
-continue;
-printf("\n");
-}
-if (size <= 0)
-printf("\n");
+	for (byte = 0; byte < size; byte += 10)
+	{
+		printf("%08x: ", byte);
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + byte) >= size)
+				printf("  ");
+			else
+				printf("%02x", *(b + i + byte));
+			if ((i % 2) != 0 && i != 0)
+				printf(" ");
+		}
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + byte) >= size)
+				break;
+			else if (*(b + i + byte) >= 31 && *(b + i + byte) <= 126)
+				printf("%c", *(b + i + byte));
+			else
+				printf(".");
+		}
+		if (byte >= size)
+			continue;
+		printf("\n");
+	}
+	if (size <= 0)
+		printf("\n");
 }
