f3a046e55a77bfbadbe5887b81abe3d0db0bd608
Abiodun Gafar Shittu <69143577+Abiodun-Shittu@users.noreply.github.com>
2022-07-15 02:02:05 +0100

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,10 +14,12 @@ int i, j;
 for (i = 0; dest[i] != '\0'; i++)
 {
 }
-for (j = 0; src[j] != '\0'; j++, i++)
+
+for (j = 0; src[j]; j++, i++)
 {
 dest[i] = src[j];
 }
 dest[i] = '\0';
+
 return (dest);
 }
