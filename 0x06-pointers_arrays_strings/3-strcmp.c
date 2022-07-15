fd628a744ecce6a75dd4ba6d8075c11a6e1c0522
Abiodun Gafar Shittu <69143577+Abiodun-Shittu@users.noreply.github.com>
2022-07-15 02:14:58 +0100

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,20 +7,17 @@
 *
 * Return: 0 if similar, positive number if s1 > s2, otherwise negative number
 */
-
 int _strcmp(char *s1, char *s2)
 {
-while((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+int i;
+
+for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 {
-s1++;
-s2++;
-}
-if(*s1 == *s2)
-{
-return (0);
+if (s1[i] != s2[i])
+return (s1[i] - s2[i]);
 }
 else
 {
-return (*s1 - *s2);
+return (0);
 }
 }
