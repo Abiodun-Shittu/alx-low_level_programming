22febae97a1a870cceb60317a61996447601d5a5
Abiodun Gafar Shittu <69143577+Abiodun-Shittu@users.noreply.github.com>
2022-07-19 00:25:14 +0100

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,7 +5,7 @@
  * @haystack: This is the input string
  * @needle: This is a find to locate in the string
  * Return: a pointer to the beginning of the located substring, or NULL if the
- * substring is not found.
+ * substring is not found
  */
 
 char  *_strstr(char *haystack, char *needle)
