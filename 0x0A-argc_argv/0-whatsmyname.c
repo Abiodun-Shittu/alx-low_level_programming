50d48c41fda8a4dd030ab1cf44d6d890788793cd
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-07-22 10:19:19 -0700

    What's my name?

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char **argv)
+{
+	printf("name = %s\n", argv[0]);
+	return (0);
+}
