a3bcb1517e8f5e7705f3d743625011c3ed6ffff4
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-07-26 15:59:42 -0700

    Concatenate String

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,19 +16,28 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
-	for (len1 = 0; s1[len1] != '\0'; len1++)
 	if (s2 == NULL)
 		s2 = "";
+
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
 	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
 
 	len = len1 + len2;
 	conStr = malloc(sizeof(char) * (len + 1));
+
 	if (conStr == NULL)
+	{
+		free(conStr);
 		return (NULL);
+	}
 	for (i = 0; i < len1; i++)
 		conStr[i] = s1[i];
+
 	j = len2;
 	for (len2 = 0; len2 <= j; i++, len2++)
 		conStr[i] = s2[len2];
+
 	return (conStr);
 }
