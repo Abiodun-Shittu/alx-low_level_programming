14ffa96e88c9c75cf0340a64ace469ffa9774022
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-07-26 17:50:28 -0700

    concatenates all the arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -21,7 +21,7 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j]; j++)
 			len++;
 
 	str = malloc((len + ac + 1) * sizeof(char));
