b18d4f757dcc3dfcb4714b1db90c6a6b942ae6e8
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-07-28 15:41:55 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,6 +18,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
+	if (ptr == NULL)
+		return (malloc(new_size));
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
