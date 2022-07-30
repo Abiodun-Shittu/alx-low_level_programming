3d21c4fe33ba362f4dcdcf256446ceeb21b66cde
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-07-29 18:59:38 -0700

    update header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
