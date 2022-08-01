03d2a981610fdeaf22f2a799f025ee53051c9b90
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-08-01 16:22:05 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the name of the file
+ * it was compiled from
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
