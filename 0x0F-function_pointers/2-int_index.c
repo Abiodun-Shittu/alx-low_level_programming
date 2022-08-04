b119e518f2db743aecee2cd24eb2b9d4c8c63026
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-08-04 06:05:19 -0700

    I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer in @array
+ * @array: pointer to the array of numbers.
+ * @size: size of array.
+ * @cmp: pointer to comparation function
+ * Return: the index of array.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (!array || !cmp)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+}
