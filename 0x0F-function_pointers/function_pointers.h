30c13aa8f3f6e15b271641256c2d0cca770d9afe
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-08-04 03:15:51 -0700

    Header file

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef _FUNC_PTR_H_
+#define _FUNC_PTR_H_
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
+
+#endif
