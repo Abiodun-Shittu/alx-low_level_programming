855f26e08718da0f3ff5923ae7585fc066ac0109
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-08-04 10:56:44 -0700

    Simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,6 +8,8 @@
 
 int (*get_op_func(char *s))(int, int)
 {
+	int i;
+
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -16,7 +18,6 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
 
 	for (i = 0; i < 5; i++)
 	{
