dca582223a97b76d0aa904dde296b76dc92d21f6
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-08-04 11:17:37 -0700

    Header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef _VAR_FUNC_H_
+#define _VAR_FUNC_H_
+
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
