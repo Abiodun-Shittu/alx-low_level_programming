57bbcb8ab9b029017d784d03b5c3652d3577c5a3
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-08-04 05:46:45 -0700

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,12 +5,10 @@
  *
  * @name: pointer to Name's string.
  * @f: pointer to function.
- *
- * Return: the string initialized.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-		return (f(name));
+		f(name);
 }
