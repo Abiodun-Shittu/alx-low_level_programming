e18e291944fcda062c8a28b8bf93600eea5bfb91
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-08-04 05:45:01 -0700

    Update header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef _FUNC_PTR_H_
 #define _FUNC_PTR_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
