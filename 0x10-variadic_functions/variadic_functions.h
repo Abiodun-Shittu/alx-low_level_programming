c09793b892186ac5398a1ea7a42f25dd6b7c9e71
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-08-04 11:19:18 -0700

    Header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,7 @@
 #define _VAR_FUNC_H_
 
 #include <stdarg.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
