bc6af5882acf71a43d840468a890e63e9347ac4b
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-08-04 10:14:18 -0700

    Simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+
+/**
+ * main - operates two numbers.
+ * @argc: argument counter.
+ * @argv: argument vector.
+ * Return: result, otherwise 98 for arg error, 99 for op, error 100 for divide
+ * or multiply by 0
+ */
+
+int main(int argc, char **argv)
+{
+	int num1, num2, answer;
+	int (*function)(int, int);
+
+	if (argc != 4)
+	{
+		puts("Error");
+		exit(98);
+	}
+
+	if (argv[2][1] != '\0')
+	{
+		puts("Error");
+		exit(99);
+	}
+
+	function = get_op_func(*(argv + 2));
+	if (!function)
+	{
+		puts("Error");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	answer = function(num1, num2);
+	printf("%d\n", answer);
+	return (0);
+}
