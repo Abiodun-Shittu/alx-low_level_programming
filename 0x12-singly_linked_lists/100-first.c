011f2859a9c7f0d144e229551b4152f05fa495ff
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-08-11 16:42:29 +0100

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * printBefore - function that gets executed before main
+ */
+void __attribute__ ((constructor)) printBefore()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
