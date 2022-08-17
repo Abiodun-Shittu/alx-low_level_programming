ea393c231d9c0fc6548a6c26e5dee920ac87f16f
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-08-18 00:19:30 +0100

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,11 +8,16 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t i;
+	const listint_t *temp;
 
-	for (i = 0; h != NULL; i++)
+	if (h == NULL)
+		return (NULL);
+
+	temp = h;
+	for (i = 0; temp != NULL; i++)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", temp->n);
+		temp = temp->next;
 	}
 	return (i);
 }
