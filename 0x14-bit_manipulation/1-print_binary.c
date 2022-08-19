a2011b295b4a0a2ada897748e7bc8a66b4e59344
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-08-20 00:56:06 +0100

    binary representation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_binary - function that prints the binary representation
+ * of a number.
+ * @n: the unsigned long int.
+ * Return: no return.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+		_putchar('0');
+}
