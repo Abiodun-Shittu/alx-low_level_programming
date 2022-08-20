1c124f49b1b9cf2dc03e6acad3b1c16d360e937d
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-08-20 01:12:29 +0100

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,29 +7,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	int i;
+	unsigned int num = 0;
 
 	if (!b)
 		return (0);
 
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (i = 0; *(b + i) != '\0'; i++)
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
+		if (*(b + i)  == '1')
+			num = (num << 1) | 1;
+		else if (*(b + i) == '0')
+			num <<= 1;
+		else
 			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
 	}
-
-	return (ui);
+	return (num);
 }
