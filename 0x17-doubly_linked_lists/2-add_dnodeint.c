ae8a1189cf65ba16e97ebf38785617ac3aa52029
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-09-30 07:38:13 +0100

    Add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,9 +17,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->next = *head;
 	new_node->prev = NULL;
 	if (*head != NULL)
-		*head->prev = new_node;
+		(*head)->prev = new_node;
 
 	*head = new_node;
-
 	return (new_node);
 }
