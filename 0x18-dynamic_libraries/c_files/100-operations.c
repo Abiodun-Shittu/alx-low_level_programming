7bbc2983b5d721e310a10f521e6624410db69153
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-10-04 04:26:38 +0100

    test files

---
+++ 0x18-dynamic_libraries/c_files/100-operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - function to add two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - function to subtract two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - function to multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - function to add divide integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - function to find the remainder of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
