b6f98b09544c4ee301beb29390397d556982defd
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-10-04 04:41:37 +0100

    Code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Abiodun-Shittu/alx-low_level_programming/master/0x18-dynamic_libraries/lib.so
-export LD_PRELOAD=/tmp/lib.so
+wget -P /tmp/ https://raw.github.com/Abiodun-Shittu/alx-low_level_programming/master/0x18-dynamic_libraries/libruin.so
+export LD_PRELOAD=/tmp/libruin.so
