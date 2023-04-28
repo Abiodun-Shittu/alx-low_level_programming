f819a2d7c8e111bd088c407aa1c8d8b2cddda62b
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-04-28 21:10:36 +0100

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main- C program that prints to the standard output followed by a new line
+ * Return: Always Zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
